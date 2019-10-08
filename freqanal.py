3e405d6addadd3cecb5260938966083a400cbef0
esohlberg <esohlberg@uchicago.edu>
2019-10-07 22:11:41 -0500

    finished freqanal

--- freqanal.py
+++ freqanal.py
@@ -1,12 +1,28 @@
 import argparse
+import os
 
 
 if __name__ == '__main__':
-  files = [f for f in os.listdir('.') if os.path.isfile(f)]
-  for f in files:
-    print(f)
-  parser = argparse.ArgumentParser()
-  parser.add_argument("filename")
-  args = parser.parse_args()
-  #f = open('ctxts\' + args.filename, 'r')
-  #print(f.read())
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    args = parser.parse_args()
+    filename = 'ctxts/' + args.filename
+    x = open(filename, 'r').read()
+    print(x)
+    allchars = {}
+    print(x)
+    for char in x:
+        if char in allchars:
+            allchars[char] += 1
+        else: 
+            allchars[char] = 1
+    while allchars:
+        highest = 0
+        highestkey = None
+        for key, value in allchars.items():
+            if value > highest:
+                highest = value
+                highestkey = key
+        if (highestkey.isalnum()):
+            print(highestkey + ': ' + str(highest))
+        allchars.pop(highestkey, None)        
