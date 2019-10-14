e1eeb2ea369314f11ba11797bfdcd43f3bc0eafa
esohlberg <esohlberg@uchicago.edu>
2019-10-14 13:23:57 -0500

    added vignersolver

---
+++ vignersolver.py
@@ -0,0 +1,38 @@
+import argparse
+import os
+
+def adjustchar(currchar, keychar):
+    if currchar == ' ':
+        return ' '
+    currnum = ord(currchar) - 96
+    keynum = ord(keychar) - 96
+    newnum = currnum + keynum
+    if newnum > 26:
+        newnum = (newnum % 26)
+    if newnum == 0:
+        newnum = 26
+    newnum = newnum + 96
+    return chr(newnum)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    parser.add_argument("key")
+    args = parser.parse_args()
+    filename = 'ctxts/' + args.filename
+    key = args.key
+    x = open(filename, 'r').read()
+    answer = ''
+    keyspot = 0
+    while len(x) > 1:
+        currchar = x[0]
+        x = x[1:]
+        answer += adjustchar(currchar, key[keyspot])
+        keyspot += 1
+        if keyspot == len(key):
+            keyspot = 0
+    print(answer)
+
+
+
+
